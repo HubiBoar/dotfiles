438ded6c46dfe723b74d6f143bbf240b22420a80
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2024-08-13 11:50:56 +0000

    Simplify workflow

--- devdocker/modules/runner/config.sh
+++ devdocker/modules/runner/config.sh
@@ -1,9 +1,6 @@
 echo "\n --> Downloading dotfiles from github...\n"
 git clone --depth=1 https://github.com/HubiBoar/dotfiles ~/dotfiles
 
-echo "\n --> Clearing previous .gitconfig...\n"
-rm ~/.gitconfig -f
-
 echo "\n --> Stow adopt downloaded dotfiles...\n"
 stow --dir="$HOME/dotfiles" --adopt .
 
@@ -11,4 +8,4 @@ echo "\n --> Installing Tmux plugins...\n"
 ~/.tmux/plugins/tpm/bin/install_plugins
 
 echo "\n --> Setting up Nvim"
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin" ' >> ~/.zshrc \
+echo 'export PATH="$PATH:/opt/nvim-linux64/bin" ' >> ~/.zshrc
