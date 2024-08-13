32a911fbabb2a31bec92424347d8124f73fdcb7c
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2024-08-13 09:37:46 +0000

    mend

--- devdocker/runner/run.sh
+++ devdocker/runner/run.sh
@@ -2,6 +2,9 @@
 git clone --depth=1 https://github.com/HubiBoar/dotfiles ~/dotfiles
 stow --dir="$HOME/dotfiles" --adopt .
 
+#tmux plugins
+~/.tmux/plugins/tpm/bin/install_plugins
+
 #git and github ssh
 chmod -R 400 ~/.ssh
 gh auth login --with-token < ~/.ssh/.githubtoken
