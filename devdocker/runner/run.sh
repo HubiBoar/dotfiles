e7c654855ef033a7e29d17b65b9e7155c82354d0
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2024-08-13 09:29:06 +0000

    mend

--- devdocker/runner/run.sh
+++ devdocker/runner/run.sh
@@ -1,5 +1,11 @@
+#dotfiles and stow
 git clone --depth=1 https://github.com/HubiBoar/dotfiles ~/dotfiles
+stow --dir="$HOME/dotfiles" --adopt .
+
+#git and github ssh
 chmod -R 400 ~/.ssh
 gh auth login --with-token < ~/.ssh/.githubtoken
+
+#finished
 echo "[CDE Is Running] (Ctrl-c to Detach)"
 sleep infinity
