f67d58d60147775a4ff09895b3d0cff0a621ca67
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2024-08-20 08:54:53 +0000

    Folder keeping already installed configs

--- devdocker/setup/runner/config.sh
+++ devdocker/setup/runner/config.sh
@@ -1,8 +1,15 @@
-echo "\n --> Stow adopt downloaded dotfiles...\n"
-stow --dir="/startup/dotfiles/" --target=$HOME --adopt -v .
+installConfig() {
+    echo "\n --> Stow adopt downloaded dotfiles...\n"
+    stow --dir="/startup/dotfiles/" --target=$HOME --adopt -v .
 
-echo "\n --> Installing Tmux plugins... \n"
-~/.tmux/plugins/tpm/bin/install_plugins
+    echo "\n --> Installing Tmux plugins... \n"
+    ~/.tmux/plugins/tpm/bin/install_plugins
 
-echo "\n --> Setting up Nvim"
-grep -qxF 'export PATH="$PATH:/opt/nvim-linux64/bin"' ~/.zshrc || echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.zshrc
+    echo "\n --> Setting up Nvim"
+    grep -qxF 'export PATH="$PATH:/opt/nvim-linux64/bin"' ~/.zshrc || echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.zshrc
+
+    touch /installed/config.txt
+}
+
+mkdir /installed
+test -f /installed/config.txt || installConfig
