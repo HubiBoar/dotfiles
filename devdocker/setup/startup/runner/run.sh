f8818539dfe248dc9d56a1e992f00f2807a75dfa
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2024-10-21 11:24:43 +0000

    Updates for tmux ressurect state path

--- devdocker/setup/startup/runner/run.sh
+++ devdocker/setup/startup/runner/run.sh
@@ -2,5 +2,5 @@ echo "\n --> Git and Github SSH setup...\n"
 chmod 400 ~/.ssh
 gh auth login --with-token < ~/.ssh/.githubtoken
 
-echo " => CDE Is Running... (Ctrl-c to Detach. './start.sh profile' to Attach)"
+echo " => CDE Is Running... (Ctrl-c to Detach. './start.sh $PROFILE' to Attach)"
 sleep infinity
