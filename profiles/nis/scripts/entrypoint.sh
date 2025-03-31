57cb3f29d89fd2e1f4604b40f770f0ef64cc6fce
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2025-03-31 11:30:20 +0000

    Dotnet build on nis

--- profiles/nis/scripts/entrypoint.sh
+++ profiles/nis/scripts/entrypoint.sh
@@ -7,9 +7,9 @@ create_nordic_session() {
     tmux new-session -d -s "$session_name" -c "$session_path" -n zsh
 
     tmux new-window -t "$session_name":2 -c "$session_path" -n lf "source /root/.zshrc && lf; exec zsh"
-    tmux new-window -t "$session_name":3 -c "$session_path/src" -n nvim "source /root/.zshrc && nvim .; exec zsh"
-    tmux new-window -t "$session_name":3 -c "$session_path/src/Nordic/UI" -n frontend
-    tmux new-window -t "$session_name":3 -c "$session_path/src/Nordic/Nordic.WebApi" -n backend
+    tmux new-window -t "$session_name":3 -c "$session_path" -n nvim "source /root/.zshrc; echo 'Press Enter to start build...'; read; (cd $session_path/src/Nordic/Nordic.WebApi && dotnet build); nvim .; exec zsh"
+    tmux new-window -t "$session_name":4 -c "$session_path/src/Nordic/UI" -n frontend
+    tmux new-window -t "$session_name":5 -c "$session_path/src/Nordic/Nordic.WebApi" -n backend
 }
 
 create_nordic_sessions() {
@@ -26,11 +26,11 @@ create_nordic_sessions() {
     done
 }
 
-cchmod +x /scripts/*.sh
+chmod +x /scripts/*.sh
 
 source /scripts/create-session.sh
 
-/scripts/create-default-session.sh
+/scripts/create-default-sessions.sh
 
 create_session "nis-baltic" "/home/projects/nisportal/baltic"
 
