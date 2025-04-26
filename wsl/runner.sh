7b940bbe143f9926170fcd4f894981634590b659
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2025-04-26 09:34:22 +0000

    Updates

--- wsl/runner.sh
+++ wsl/runner.sh
@@ -2,8 +2,10 @@ sudo cp -rf ./etc/* /etc/
 
 #https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04#step-3-using-the-docker-command
 
+sudo cp -rf ./etc/* /etc/
+
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -y
