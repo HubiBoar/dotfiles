a6aaab81378a3db8372e168154745cc1ffdbf9f5
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2025-07-04 23:57:38 +0000

    Auto configure nvim harpoon plugin

--- nix/root/.config/nix/nvim-flake/flake.nix
+++ nix/root/.config/nix/nvim-flake/flake.nix
@@ -1,4 +1,6 @@
-#nix profile install --extra-experimental-features "nix-command flakes" path:.config/nix/nvim-flake
+#nix profile install .
+#nix profile upgrade nvim-flake
+#sha256 = "0000000000000000000000000000000000000000000000000000";
 {
   description = "Dev environment with Neovim, Git, Zsh, and more";
 
@@ -7,41 +9,81 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        neovimPkgs = import (builtins.fetchTarball {
+          #https://lazamar.co.uk/nix-versions/?package=neovim&version=0.9.5&fullName=neovim-0.9.5&keyName=neovim&revision=e89cf1c932006531f454de7d652163a9a5c86668&channel=nixpkgs-unstable#instructions
+          #nvim 0.11.2
+          url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
+          sha256 = "0m0xmk8sjb5gv2pq7s8w7qxf7qggqsd3rxzv3xrqkhfimy2x7bnx";
+        }) {
+          inherit system;
+        };
+
+        azPkgs = import (builtins.fetchTarball {
+          #https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=azure-cli
+          #az 2.71.0
+          url = "https://github.com/NixOS/nixpkgs/archive/3e2cf88148e732abc1d259286123e06a9d8c964a.tar.gz";
+          sha256 = "1gvlrbl3fx1fwyb26w5k8rdlxnhzf11si7pzf3khw9n1v4jhqdw0";
+        }) {
+          inherit system;
+        };
+
+        bicepPkgs = import (builtins.fetchTarball {
+          #https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=bicep
+          #bicep v0.33.93
+          url = "https://github.com/NixOS/nixpkgs/archive/0bd7f95e4588643f2c2d403b38d8a2fe44b0fc73.tar.gz";
+          sha256 = "0vx2pw69if88nkfh74bf1a8s5497n2nv7wydmvmqh5qh00fsahvq";
+        }) {
+          inherit system;
+        };
+
+        dotnetCombined = pkgs.dotnetCorePackages.combinePackages [
+            pkgs.dotnetCorePackages.sdk_8_0
+            pkgs.dotnetCorePackages.sdk_9_0
+          ];
+
       in {
         packages.default = pkgs.buildEnv {
           name = "full-dev-env";
           paths = with pkgs; [
             # Core tools
-            zsh
-            git
-            curl
-            inetutils  # includes `ping`
-            gh
-            zip
-            unzip
-            wget
-            iproute2
-            jq
+            pkgs.zsh
+            pkgs.git
+            pkgs.curl
+            pkgs.inetutils  # includes `ping`
+            pkgs.gh
+            pkgs.zip
+            pkgs.unzip
+            pkgs.wget
+            pkgs.iproute2
+            pkgs.jq
 
             # Editors & tools
-            neovim
-            ripgrep
-            fd
-            tmux
+            neovimPkgs.neovim
+            pkgs.ripgrep
+            pkgs.fd
+            pkgs.tmux
 
             # Clipboard
-            xclip
+            pkgs.xclip
 
             # Extras
-            docker
-            lf
-            lazysql
+            pkgs.docker
+            pkgs.lf
+            pkgs.lazysql
+
+            azPkgs.azure-cli
+            bicepPkgs.bicep
+
+            dotnetCombined
+            pkgs.nodejs
+            pkgs.nodePackages.npm
           ];
 
           # Optional: ensure `man` pages and bin links are preserved
--- profiles/base/root/.config/nvim/lua/plugins/harpoon.lua
+++ profiles/base/root/.config/nvim/lua/plugins/harpoon.lua
@@ -1,5 +1,19 @@
-local harpoon = require("harpoon")
+local path = vim.fn.stdpath("config") .. "/pack/plugins/start/harpoon"
+local commit_sha = "ed1f853847ffd04b2b61c314865665e1dadf22c7" -- replace with your desired commit SHA
 
+if not (vim.uv or vim.loop).fs_stat(path) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/ThePrimeagen/harpoon.git",
+    path,
+  })
+  vim.fn.system({ "git", "-C", path, "checkout", commit_sha })
+end
+vim.opt.rtp:prepend(path)
+
+local harpoon = require("harpoon")
 harpoon:setup()
 
 local conf = require("telescope.config").values
@@ -20,3 +34,4 @@ local function toggle_telescope(harpoon_files)
 end
 
 require("../keys").harpoon(harpoon, toggle_telescope)
+
