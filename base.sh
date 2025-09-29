acc3066959111c06af049eec2edfe74b7d78aebd
hubert <hubiboar.studio@gmail.com>
2025-09-29 14:26:53 +0200

    Support linux

---
+++ base.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/projects;
+mkdir -p ~/.docker;
+mkdir -p ~/.password-store;
+mkdir -p ~/.gnupg;
+
+mkdir -p ./profiles/${PROFILE}/state;
+mkdir -p ./profiles/${PROFILE}/state/root;
+touch ./profiles/${PROFILE}/lazy-lock.json;
--- dotnet.sh
+++ dotnet.sh
@@ -1,3 +1,9 @@
-PROFILE=dotnet docker compose build base || exit 1
-PROFILE=dotnet docker compose build dotnet || exit 1
-PROFILE=dotnet docker compose run --rm dotnet
+export PROFILE=dotnet
+
+chmod +x base.sh
+
+./base.sh
+
+docker compose build base || exit 1
+docker compose build dotnet || exit 1
+docker compose run --rm dotnet
